f1dd10c423598be4bae7db4e6df31f693247c97f
Ali-Assaad <aliassaadtr@gmail.com>
2023-02-24 03:28:56 +0200

    Initialize variables

---
+++ index.js
@@ -0,0 +1,6 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
